fb537bcb52e539029c4567b9b64ff4611e7f08a5
Meg Welch <welch1.megan@gmail.com>
2025-07-06 14:56:43 -0500

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "../amplify/data/resource";
-import { useAuthenticator } from "@aws-amplify/ui-react";
+import { useAuthenticator } from "@aws-amplify/ui-react"; // 
 
 const client = generateClient<Schema>();
 
 function App() {
-  const { signOut, user } = useAuthenticator();
+  const { user, signOut } = useAuthenticator(); // 
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -14,14 +14,11 @@ function App() {
       next: (data) => setTodos([...data.items]),
     });
 
-    return () => subscription.unsubscribe(); // Clean up on unmount
+    return () => subscription.unsubscribe(); // 
   }, []);
 
   function createTodo() {
-    const content = window.prompt("Todo content");
-    if (content) {
-      client.models.Todo.create({ content });
-    }
+    client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
   function deleteTodo(id: string) {
@@ -30,25 +27,33 @@ function App() {
 
   return (
     <main>
-	<h1>Welcome, {user?.username}!</h1> {/* Show username */}
-	<button onClick={signOut}>Sign out</button> {/* Sign out button */}
-        <h2>My Todos</h2> {/* Add Todos title */}
-        <button onClick={createTodo}>+ new</button>
-        <ul>
-          {todos.map((todo) => (
+      {/* Render the user's username (loginId) in the header */}
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+
+      {/* Sign out button */}
+      {user && (
+        <button onClick={signOut}>Sign Out</button>
+      )}
+
+      {/* Todo creation button */}
+      <button onClick={createTodo}>+ new</button>
+
+      <ul>
+        {todos.map((todo) => (
           <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
             {todo.content}
           </li>
         ))}
       </ul>
+
       <div>
-        App successfully hosted. Try creating a new todo.
+        🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/">Review next </a>
-      </div>    
+        <a href="https://docs.amplify.aws/react/start/quickstart/">Review next &gt;</a>
+      </div>
     </main>
   );
 }
-   
+
 export default App;
 
